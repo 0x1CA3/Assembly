653bec51f1e2213cda9f25bcce3a5a2c00be1b29
0x1CA3 <86132648+0x1CA3@users.noreply.github.com>
2021-10-17 02:56:58 -0700

    Add files via upload

---
+++ while_loops/main.asm
@@ -0,0 +1,24 @@
+global _start
+
+section .text
+_start:
+example:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, string
+	mov rdx, length
+	syscall
+	cmp rax, 1
+	call _clear
+	jmp example
+
+_clear:
+	xor rax, rax
+	xor rdi, rdi
+	xor rsi, rsi
+	xor rdx, rdx
+	ret
+
+section .data
+	string: db "This will go on forever...", 0x0a, 0x00
+	length: equ $ - string
